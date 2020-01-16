0572a50885710ce37ab7710eb585da3fe9e40524
Novák Tamás <novaktamas27@gmail.com>
2020-01-16 21:00:46 +0100

    Add files via upload

---
+++ FourDimensionalSpace/ThreeD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourDimensionalSpace
+{
+    class ThreeD
+    {
+      public List<Voxel> space = new List<Voxel>();
+
+        public Voxel GetVoxel(int index)
+        {
+            Voxel vx = space[index];
+            return vx;
+        }
+
+        public bool GetData(int X,int Y, int Z)
+        {
+            Voxel vx = new Voxel(X, Y, Z);
+            bool exist = false;
+            return exist;
+        }
+    }
+    class Voxel : ThreeD 
+    {
+          public  int PX, PY, PZ;
+
+
+        public Voxel(int X, int Y, int Z)
+        {
+
+            PX = X;
+            PY = Y;
+            PZ = Z;
+        }
+    }
+}
