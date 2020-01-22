a79c7e75837428fd672ff38f99a8b692dd0f83dc
Novák Tamás <novaktamas27@gmail.com>
2020-01-22 17:25:22 +0100

    Add files via upload

--- FourDimensionalSpace/Form1.Designer.cs
+++ FourDimensionalSpace/Form1.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.infoBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ZtrackBar3 = new System.Windows.Forms.TrackBar();
+            this.YtrackBar2 = new System.Windows.Forms.TrackBar();
+            this.XtrackBar1 = new System.Windows.Forms.TrackBar();
             this.Screen = new System.Windows.Forms.PictureBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fájlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.XStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.YStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.ZSTatus = new System.Windows.Forms.ToolStripStatusLabel();
-            this.XtrackBar1 = new System.Windows.Forms.TrackBar();
-            this.YtrackBar2 = new System.Windows.Forms.TrackBar();
-            this.ZtrackBar3 = new System.Windows.Forms.TrackBar();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.infoBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fájlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ZtrackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YtrackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XtrackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Screen)).BeginInit();
             this.statusStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.XtrackBar1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.YtrackBar2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ZtrackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -63,6 +63,56 @@
             this.panel1.Size = new System.Drawing.Size(240, 500);
             this.panel1.TabIndex = 0;
             // 
+            // infoBox
+            // 
+            this.infoBox.Location = new System.Drawing.Point(3, 365);
+            this.infoBox.Multiline = true;
+            this.infoBox.Name = "infoBox";
+            this.infoBox.ReadOnly = true;
+            this.infoBox.Size = new System.Drawing.Size(232, 130);
+            this.infoBox.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 173);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(232, 186);
+            this.listBox1.TabIndex = 1;
+            // 
+            // ZtrackBar3
+            // 
+            this.ZtrackBar3.Location = new System.Drawing.Point(3, 122);
+            this.ZtrackBar3.Maximum = 250;
+            this.ZtrackBar3.Minimum = -250;
+            this.ZtrackBar3.Name = "ZtrackBar3";
+            this.ZtrackBar3.Size = new System.Drawing.Size(232, 45);
+            this.ZtrackBar3.TabIndex = 0;
+            this.ZtrackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.ZtrackBar3.ValueChanged += new System.EventHandler(this.Trackbar_changed);
+            // 
+            // YtrackBar2
+            // 
+            this.YtrackBar2.Location = new System.Drawing.Point(3, 71);
+            this.YtrackBar2.Maximum = 250;
+            this.YtrackBar2.Minimum = -250;
+            this.YtrackBar2.Name = "YtrackBar2";
+            this.YtrackBar2.Size = new System.Drawing.Size(232, 45);
+            this.YtrackBar2.TabIndex = 0;
+            this.YtrackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.YtrackBar2.ValueChanged += new System.EventHandler(this.Trackbar_changed);
+            // 
+            // XtrackBar1
+            // 
+            this.XtrackBar1.Location = new System.Drawing.Point(3, 20);
+            this.XtrackBar1.Maximum = 250;
+            this.XtrackBar1.Minimum = -250;
+            this.XtrackBar1.Name = "XtrackBar1";
+            this.XtrackBar1.Size = new System.Drawing.Size(232, 45);
+            this.XtrackBar1.TabIndex = 0;
+            this.XtrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.XtrackBar1.ValueChanged += new System.EventHandler(this.Trackbar_changed);
+            // 
             // Screen
             // 
             this.Screen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
@@ -71,6 +121,7 @@
             this.Screen.Size = new System.Drawing.Size(500, 500);
             this.Screen.TabIndex = 1;
             this.Screen.TabStop = false;
+            this.Screen.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Screen_MouseMove);
             // 
             // statusStrip1
             // 
@@ -83,22 +134,6 @@
             this.statusStrip1.Size = new System.Drawing.Size(784, 22);
             this.statusStrip1.TabIndex = 2;
             this.statusStrip1.Text = "statusStrip1";
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fájlToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fájlToolStripMenuItem
-            // 
-            this.fájlToolStripMenuItem.Name = "fájlToolStripMenuItem";
-            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fájlToolStripMenuItem.Text = "Fájl";
             // 
             // XStatus
             // 
@@ -118,49 +153,21 @@
             this.ZSTatus.Size = new System.Drawing.Size(17, 17);
             this.ZSTatus.Text = "Z:";
             // 
-            // XtrackBar1
+            // menuStrip1
             // 
-            this.XtrackBar1.Location = new System.Drawing.Point(3, 20);
-            this.XtrackBar1.Maximum = 500;
-            this.XtrackBar1.Name = "XtrackBar1";
-            this.XtrackBar1.Size = new System.Drawing.Size(232, 45);
-            this.XtrackBar1.TabIndex = 0;
-            this.XtrackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fájlToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // YtrackBar2
+            // fájlToolStripMenuItem
             // 
-            this.YtrackBar2.Location = new System.Drawing.Point(3, 71);
-            this.YtrackBar2.Maximum = 500;
-            this.YtrackBar2.Name = "YtrackBar2";
-            this.YtrackBar2.Size = new System.Drawing.Size(232, 45);
-            this.YtrackBar2.TabIndex = 0;
-            this.YtrackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
-            // 
-            // ZtrackBar3
-            // 
-            this.ZtrackBar3.Location = new System.Drawing.Point(3, 122);
-            this.ZtrackBar3.Maximum = 500;
-            this.ZtrackBar3.Name = "ZtrackBar3";
-            this.ZtrackBar3.Size = new System.Drawing.Size(232, 45);
-            this.ZtrackBar3.TabIndex = 0;
-            this.ZtrackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(3, 173);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(232, 186);
-            this.listBox1.TabIndex = 1;
-            // 
-            // infoBox
-            // 
-            this.infoBox.Location = new System.Drawing.Point(3, 365);
-            this.infoBox.Multiline = true;
-            this.infoBox.Name = "infoBox";
-            this.infoBox.ReadOnly = true;
-            this.infoBox.Size = new System.Drawing.Size(232, 130);
-            this.infoBox.TabIndex = 2;
+            this.fájlToolStripMenuItem.Name = "fájlToolStripMenuItem";
+            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fájlToolStripMenuItem.Text = "Fájl";
             // 
             // MainWindow
             // 
@@ -175,17 +182,19 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FourDimensionalSpace";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ZtrackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YtrackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XtrackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Screen)).EndInit();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.XtrackBar1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.YtrackBar2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ZtrackBar3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,7 +207,6 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.TrackBar ZtrackBar3;
         private System.Windows.Forms.TrackBar YtrackBar2;
-        private System.Windows.Forms.TrackBar XtrackBar1;
         private System.Windows.Forms.PictureBox Screen;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel XStatus;
@@ -206,6 +214,7 @@
         private System.Windows.Forms.ToolStripStatusLabel ZSTatus;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fájlToolStripMenuItem;
+        private System.Windows.Forms.TrackBar XtrackBar1;
     }
 }
 
--- FourDimensionalSpace/Form1.cs
+++ FourDimensionalSpace/Form1.cs
@@ -12,9 +12,88 @@ namespace FourDimensionalSpace
 {
     public partial class MainWindow : Form
     {
+        ThreeD td = new ThreeD();
+        Camera cam = new Camera(0,0,0,0,0,0,2);
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Screen.MouseWheel += Screen_MouseWheel;
+        }
+
+
+
+        public void Trackbar_changed(object sender, EventArgs e)
+        {
+            XStatus.Text = "X: " + XtrackBar1.Value;
+            YStatus.Text = "Y: " + YtrackBar2.Value;
+            ZSTatus.Text = "Z: " + ZtrackBar3.Value;
+            cam.XPOV = XtrackBar1.Value;
+            cam.YPOV = YtrackBar2.Value;
+            cam.ZPOV = ZtrackBar3.Value;
+
+            Showgrp(); 
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+
+            Reading();
+            Showgrp();
+            
+        }
+        public void Reading()
+        {
+            foreach(Voxel vx in td.space)
+            {
+                infoBox.Text += "X: " + vx.PX + " Y: " + vx.PY + " Z: " + vx.PZ + Environment.NewLine ;
+                   }
+        }
+
+        public void Showgrp()
+        {
+            Voxel vx1, vx2,vx3,vx4;
+            vx1 = new Voxel(0,0,0);
+            vx2 = new Voxel(100,0,0);
+            vx3 = new Voxel(100,0,100);
+            vx4 = new Voxel(0,0,100);
+            //négyzet
+            Bitmap bmp = new Bitmap(Screen.Width, Screen.Height);
+            Graphics grp = Graphics.FromImage(bmp);
+            grp.DrawLine(Pens.Black, td.Computing(vx1, cam), td.Computing(vx2, cam));
+            grp.DrawLine(Pens.Black, td.Computing(vx2, cam), td.Computing(vx3, cam));
+            grp.DrawLine(Pens.Black, td.Computing(vx3, cam), td.Computing(vx4, cam));
+            grp.DrawLine(Pens.Black, td.Computing(vx1, cam), td.Computing(vx4, cam));
+            Screen.Image = bmp;
+            infoBox.Text = td.Computing(vx3, cam).ToString();
+        }
+        private void Screen_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+
+                XtrackBar1.Value = e.X-250;
+                YtrackBar2.Value = e.Y-250;
+            }
+            catch
+            {
+            }
+        }
+        private void Screen_MouseWheel(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (e.Delta > 0)
+                {
+                    ZtrackBar3.Value += 5;
+                                    }
+                else
+                {
+                    ZtrackBar3.Value -= 5;
+                }
+            }
+            catch { }
         }
     }
 }
